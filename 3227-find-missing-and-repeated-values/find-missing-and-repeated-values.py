29e24081a1b9d95395aed74ce90794b8cf556d9a
Aranya Mallik <aranyamallik@gmail.com>
2026-07-30 23:53:02 +0530

    Time: 3 ms (90.87%) | Memory: 19.6 MB (76.45%) - LeetSync

--- 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.py
+++ 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.py
@@ -1,20 +1,59 @@
 class Solution:
     def findMissingAndRepeatedValues(self, grid: List[List[int]]) -> List[int]:
-        s = len(grid)
-        total_number = s*s
-        arr = {}
+        N = len(grid)
+        total_elements = N * N
+        
+        SN = (total_elements * (total_elements + 1)) // 2
+        SN2 = (total_elements * (total_elements + 1) * (2 * total_elements + 1)) // 6
+        
+        S = 0
+        S2 = 0
+        
         for row in grid:
-            for num in row:
-                arr[num] = arr.get(num, 0) + 1
-        
-        repeating = 0
-        missing = 0
-        
-        for num in range(1, total_number + 1):
-            if num in arr:
-                if arr[num] == 2:
-                    repeating = num
-            else:
-                missing = num
+            for val in row:
+                S += val
+                S2 += val * val
 
-        return [repeating, missing]
+        val1 = S - SN
+        val2 = S2 - SN2
+        val2 = val2 // val1
+
+        x = (val1 + val2) // 2
+        y = x - val1
+        
+        return [x, y]
+
+
+        # xr = 0
+        
+        # for row in grid:
+        #     for val in row:
+        #         xr = xr ^ val
+
+        # for num in range(1, total_elements + 1):
+        #     xr = xr ^ num
+
+        # bitNo = xr & ~(xr - 1)
+        
+        # zero = 0
+        # one = 0
+        
+        # for row in grid:
+        #     for val in row:
+        #         if val & bitNo:
+        #             one = one ^ val
+        #         else:
+        #             zero = zero ^ val
+                    
+        # for num in range(1, total_elements + 1):
+        #     if num & bitNo:
+        #         one = one ^ num
+        #     else:
+        #         zero = zero ^ num
+
+        # for row in grid:
+        #     for val in row:
+        #         if val == zero:
+        #             return [zero, one]
+                    
+        # return [one, zero]
